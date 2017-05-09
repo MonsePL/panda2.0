50fe22e3076f8106083ff346366c95d954812e5f
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-05-09 17:02:30 -0500

    version js

--- panda20.js
+++ panda20.js
@@ -1,1 +1,11 @@
-alert("hola")
+
+var imgTotales = document.getElementsByClassName('img');
+var eliminarElem = document.getElementsByClassName('quitar');
+
+
+for (var i = 0; i < eliminarElem.length; i++) {
+  imgTotales[i].addEventListener("click",ocultarImagen);
+
+  function ocultarImagen() {
+    this.style.display = "none";
+  }
